30ed5c69e4be3ef4038926f127f6521c8ad81cf3
od86 <178620777+od86@users.noreply.github.com>
2026-03-13 14:53:23 +0000

    Added js

---
+++ brandon.js
@@ -0,0 +1,12 @@
+function getTimeAndDate() {
+  let date = new Date;
+  
+  day = formatTimeAndDate(date.getDate());
+  month = formatTimeAndDate(date.getMonth() + 1);
+}
+
+function formatTimeAndDate(num) {
+  return num > 10 ? num : `0${num}`;
+}
+
+getTimeAndDate();
